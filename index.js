3cfc9d14c833e8d8b5461e81fe0384743e990f07
Angel S.E <pluslimusic@gmail.com>
2022-02-16 20:16:22 +0100

    v1.9

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ io.on("connection", (socket) => {
 
 
   socket.on('nuevo usuario', function(nombre){
+    usuarios.push(nombre);
     var coincidencia = 0;
     for (var valor of usuarios) {
       if(valor == nombre){
